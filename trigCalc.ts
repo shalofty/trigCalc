3454db72efbd85be311152e634d6f2dd534f651b
Stephan <stephanhaloftis@gmail.com>
2022-10-19 21:26:49 -0400

    Finished right angle menu

--- trigCalc.ts
+++ trigCalc.ts
@@ -67,52 +67,166 @@ function cosecant(legA:number, legB:number) {
 
 // User Interface, using prompt-sync (npm install prompt-sync)
 
-let uiPrompt:number = 0;
-const ui = require('prompt-sync')({sigint: true});
+let uxPrompt:boolean = true;
+const ux = require('prompt-sync')({sigint: true});
 
-while (uiPrompt < 1) {
+while (uxPrompt) {
     console.log("\nShalofty Trigonometry Calculator\n" +
         "Select a function:\n" +
         "1) Conversions\n" +
         "2) Right Angle Trig Functions\n" +
         "0) Exit\n")
-    let selection = ui(">>>").toString();
+    let selection = ux(">>>").toString();
     // Exit
     console.log(selection);
     if (selection == '0') {
         console.log("See ya!");
-        uiPrompt += 1;
+        uxPrompt = false;
     }
     // Conversions
     else if (selection == '1') {
         console.log('\nSelection a conversion:\n' +
             '1) Degrees to Radians\n' +
             '2) Radians to Degrees\n');
-        let conversionSelect = ui(">>>").toString();
+        let conversionSelect = ux(">>>").toString();
         if (conversionSelect == '1') {
-            let degreesInput = ui("Enter Degrees >>>");
+            let degreesInput = ux("Enter Degrees >>>");
             let degrees = +degreesInput;
             console.log("Radians: " + degreesToRadians(degrees));
         }
         else if (conversionSelect == '2') {
-            let radians:Radians = ui("Enter Radians >>>");
+            let radians:Radians = ux("Enter Radians >>>");
             console.log("Degrees: " + radiansToDegrees(radians));
         }
     }
     // Right Angle Functions
     else if (selection == '2') {
-        console.log("\nSelect a function:\n" +
+        let raPrompt:boolean = true;
+        while (raPrompt) {console.log("\nSelect a function:\n" +
             "1) Sine\n" +
             "2) Cosine\n" +
             "3) Tangent\n" +
             "4) Cotangent\n" +
             "5) Secant\n" +
-            "6) Cosecant\n");
-        let raSelect = ui(">>>").toString();
-        if (raSelect == '1') {
-            let legA = +ui("legA length >>>\n");
-            let legB = +ui("legB length >>>\n");
-            console.log(sine(legA, legB));
-        }
+            "6) Cosecant\n" +
+            "0) Exit");
+            let raSelect = ux(">>>").toString();
+            // sine() function
+            if (raSelect == '1') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(sine(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {
+                        console.log("Err: sine() function")
+                    }
+                }
+            }
+            // cosine() function
+            else if (raSelect == '2') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(cosine(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {
+                        console.log("Err: cosine() function");
+                    }
+                }
+            }
+            // tangent() function
+            else if (raSelect == '3') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(tangent(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {
+                        console.log("Err: tangent() function");
+                    }
+                }
+            }
+            // cotangent() function
+            else if (raSelect == '4') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(cotangent(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {console.log("Err: cotangent() function");}
+                }
+            }
+            // secant() function
+            else if (raSelect == '5') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(secant(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {console.log("Err: secant() function");}
+                }
+            }
+            // cosecant() function
+            else if (raSelect == '6') {
+                let doMore:boolean = true;
+                while (doMore) {
+                    let legA = +ux("legA length >>>\n");
+                    let legB = +ux("legB length >>>\n");
+                    console.log(cosecant(legA, legB));
+                    let yon = ux("More? (y/n)").toString();
+                    if (yon == 'y') {
+                        doMore = true;
+                    }
+                    else if (yon == 'n') {
+                        doMore = false;
+                    }
+                    else {console.log("Err: cosecant() function");}
+                }
+            }
+            // exit menu
+            else if (raSelect == '0') {
+                raPrompt = false;
+            }
+            // error
+            else {
+                console.log("Err: trig functions");
+            }}
+
     }
 }
